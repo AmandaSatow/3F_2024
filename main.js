17abe88edb21c6586293606ab121c2a2b5d54e07
AmandaSatow <108934778+AmandaSatow@users.noreply.github.com>
2024-06-20 09:41:32 -0300

    20-06-24

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abaconteudo");
 const contador = document.querySelectorAll(".contador");
-
+const tempoObjetivo1 = new Date (2025,02,25);
+const tempoObjetivo2 = new Date (2025,05,25);
+const tempoObjetivo3 = new Date (2025,08,25);
+const tempoObjetivo4 = new Date (2025,10,25);
+let agora = new Date();
+contador[0].textContent = tempoObjetivo1 - agora
+contador[1].textContent = tempoObjetivo2 - agora
+contador[2].textContent = tempoObjetivo3 - agora
+contador[3].textContent = tempoObjetivo4 - agora
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
