0eb9253d2522d9aa0905301c6774922b2bff6c70
AmandaSatow <108934778+AmandaSatow@users.noreply.github.com>
2024-06-21 08:56:49 -0300

    21-06-24

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ const tempoObjetivo2 = new Date (2025,05,25);
 const tempoObjetivo3 = new Date (2025,08,25);
 const tempoObjetivo4 = new Date (2025,10,25);
 let agora = new Date();
-contador[0].textContent = tempoObjetivo1 - agora
+let segundos 
+let minutos
+let horas
+let dias 
+segundos = (tempoObjetivo1 - agora)/1000;
+minutos = segundos/60;
+horas = minutos/60;
+dias = horas/24;
+
+contador[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos.`
 contador[1].textContent = tempoObjetivo2 - agora
 contador[2].textContent = tempoObjetivo3 - agora
 contador[3].textContent = tempoObjetivo4 - agora
@@ -24,6 +33,3 @@ for (let i = 0; i < botoes.length; i++){
 
     }
 }
-
-
-
